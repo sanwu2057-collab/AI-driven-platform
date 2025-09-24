bfb0912c70ce33bff651de03500391343acd67bc
sanwu2057-collab <sanwu2057@gmail.com>
2025-09-24 20:46:47 +0530

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -1,1 +1,12 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
+class HeatIslandModel:
+    def __init__(self):
+        self.model = LinearRegression()
+
+    def train(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
